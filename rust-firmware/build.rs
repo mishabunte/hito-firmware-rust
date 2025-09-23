109cf0c2eda1bfdf1c28cef536ce36dd8c61e5cc
Daniil Kirillov <danik.mad@gmail.com>
2025-09-23 19:15:49 +0200

    changed path to source

--- rust-firmware/build.rs
+++ rust-firmware/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     let config = slint_build::CompilerConfiguration::new()
         .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer);
-    slint_build::compile_with_config("src/main.slint", config.clone()).unwrap();
+    slint_build::compile_with_config("src/ui/main.slint", config.clone()).unwrap();
     slint_build::print_rustc_flags().unwrap();
 }
