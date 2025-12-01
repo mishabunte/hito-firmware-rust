89e3d86c470d103174b267e10e325495a2d5262f
Daniil Kirillov <danik.mad@gmail.com>
2025-12-01 15:22:39 +0100

    removed debug logs

--- rust-firmware/src/drivers/minifb/simulator_window.rs
+++ rust-firmware/src/drivers/minifb/simulator_window.rs
@@ -320,7 +320,7 @@ impl SimulatorWindow {
             // Mouse button just released
             //println!("Mouse released");
             self.mouse_clicked = false;
-            log_info!("Mouse released");
+            //log_info!("Mouse released");
             return Some(false);
         }
         None
@@ -420,7 +420,7 @@ impl SimulatorWindow {
 
     pub fn update(&mut self) {
         if self.window.is_open() {
-            self.draw_memory_overlay();
+            //self.draw_memory_overlay();
 
             self.window.update_with_buffer(&self.buffer, WINDOW_WIDTH as usize, WINDOW_HEIGHT as usize).unwrap();
         }
