338e2425b84692386d0c5a47b819d2ecc154986c
Daniil Kirillov <danik.mad@gmail.com>
2025-12-01 15:14:26 +0100

    added sdf font option to build.rs

--- rust-firmware/build.rs
+++ rust-firmware/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     let config = slint_build::CompilerConfiguration::new()
-        .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer);
+        .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer).with_sdf_fonts(true);
     slint_build::compile_with_config("src/ui/slint/main.slint", config).unwrap();
     slint_build::print_rustc_flags().unwrap();
 
