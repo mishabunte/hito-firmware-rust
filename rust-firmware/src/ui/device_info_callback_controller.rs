ff6d353e399421aa76b27449473e93f343f37608
Daniil Kirillov <danik.mad@gmail.com>
2025-12-01 15:18:45 +0100

    changed ShowSeedScreen logic

--- rust-firmware/src/ui/device_info_callback_controller.rs
+++ rust-firmware/src/ui/device_info_callback_controller.rs
@@ -1,13 +1,15 @@
+use crate::log_info;
 use crate::{STATE, ui::CallbackController, hito_firmware::HitoFirmware};
 use crate::slint_generatedMainWindow::DeviceInfoState;
 use crate::slint_generatedMainWindow::ShowSeedState;
 use crate::slint_generatedMainWindow::MainWindow;
 extern crate alloc;
-use alloc::rc::Rc;
 use slint::SharedString;
-use slint::VecModel;
-use slint::{ComponentHandle, ModelRc};
-use crate::log_info;
+use slint::{ComponentHandle};
+use alloc::vec::Vec;
+use alloc::format;
+
+const WORDS_PER_PAGE: usize = 8;
 
 pub struct DeviceInfoCallbackController;
 
@@ -19,6 +21,25 @@ impl CallbackController for DeviceInfoCallbackController {
             s.mark_device_info_requested();
             // log_info!("Device info requested");
         });
+        let ui_weak = ui.as_weak();
+
+        ui.global::<ShowSeedState>().on_get_word(move |index| {
+            // Try to upgrade the weak handle each time the callback is called
+            if let Some(ui) = ui_weak.upgrade() {
+                let show_seed = ui.global::<ShowSeedState>();
+                let phrase = show_seed.get_seed_phrase_fragment(); // SharedString
+
+                let word = phrase
+                    .split_whitespace()
+                    .nth(index as usize)
+                    .unwrap_or("");
+
+                SharedString::from(word)
+            } else {
+                // UI was destroyed
+                SharedString::new()
+            }
+        });
     }
     fn handle_loop_events(&self, ui: &MainWindow, firmware: &mut HitoFirmware) {
         let s = STATE.get().unwrap().lock();
@@ -26,26 +47,22 @@ impl CallbackController for DeviceInfoCallbackController {
         let device_info_state = ui.global::<DeviceInfoState>();
         let show_seed_state = ui.global::<ShowSeedState>();
         if s.is_device_info_requested() {
-            // log_info!("Providing device info to UI");
-            let info = firmware.vault.get_device_info().unwrap();
-            let mnemonic = firmware.vault.get_mnemonic().expect("Error getting mnemonic");
-            // log_info!("Mnemonic retrieved: {}\n", mnemonic);    
-            // log_info!("Device info: FW ver {}, BL ver {}, SN {}, FR count {}",
-                // info.firmware_version, info.bootloader_version, info.serial_number, info.factory_reset_count);
+            let info = firmware.vault.get_device_info().unwrap();  
             device_info_state.set_firmware_version(slint::SharedString::from(&info.firmware_version[10..]));
             device_info_state.set_bootloader_version(slint::SharedString::from(&info.bootloader_version));
             device_info_state.set_serial_number(slint::SharedString::from(&info.serial_number));
-            device_info_state.set_factory_reset_count(info.factory_reset_count);
-            device_info_state.invoke_request_factory_reset_string();
-            // let the_model : Rc<VecModel<SharedString>> =
-            //     ModelRc::from(Rc::new(VecModel::from(mnemonic
-            //         .split(' ')
-            //         .map(|s| SharedString::from(s))
-            //         .collect::<alloc::vec::Vec<SharedString>>())));
-            show_seed_state.set_seed_phrase(ModelRc::from(Rc::new(VecModel::from(mnemonic
-                    .split(' ')
-                    .map(|s| SharedString::from(s))
-                    .collect::<alloc::vec::Vec<SharedString>>()))));
+            if info.factory_reset_count < 0 {
+              device_info_state.set_factory_reset_string(slint::SharedString::from("Factory reset no data"));
+            } else if info.factory_reset_count == 0  {
+              device_info_state.set_factory_reset_string(slint::SharedString::from("Factory setup"));
+            } else if info.factory_reset_count > 30 {
+              device_info_state.set_factory_reset_string(slint::SharedString::from("Factory reset 30 plus"));
+            } else {
+              device_info_state.set_factory_reset_string(slint::SharedString::from(&format!("Factory reset {} times", info.factory_reset_count)));
+            }
+            show_seed_state.set_seed_phrase_fragment(slint::SharedString::from(&firmware.vault.get_mnemonic().expect("Error getting mnemonic")));
+            show_seed_state.set_seed_len(firmware.vault.get_mnemonic_len() as i32);
+            show_seed_state.set_total_pages((firmware.vault.get_mnemonic_len() / (WORDS_PER_PAGE + 1) + 1) as i32);
             s.clear_device_info_requested();
         }
     }
