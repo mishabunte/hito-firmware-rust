07a05f29f7f99a14de2aca3cc7062a856096e3f1
Daniil Kirillov <danik.mad@gmail.com>
2025-12-05 00:05:13 +0100

    fixed second unlock bug

--- rust-firmware/src/ui/enter_pin_callback_controller.rs
+++ rust-firmware/src/ui/enter_pin_callback_controller.rs
@@ -45,11 +45,6 @@ pub struct EnterPinCallbackController;
 
 impl CallbackController for EnterPinCallbackController {
     fn register_main_window_callbacks(&self, ui: &MainWindow, firmware: &mut HitoFirmware) {
-        // PIN input mechanics
-        // let arr = [b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9'];
-        // let shuffled = shuffle_digits(arr);
-        // let shuffled_str = alloc::string::String::from_utf8_lossy(&shuffled).to_shared_string();
-        // ui.global::<EnterPinState>().set_password_sequence(shuffled_str);
         ui.global::<EnterPinState>().on_append_char(move |digit| {
             log_info!("Append char to PIN: {}", digit);
             let s = STATE.get().unwrap().lock();
@@ -90,14 +85,13 @@ impl CallbackController for EnterPinCallbackController {
         ui.global::<EnterPinState>().on_remove_char(move || {
             let s = STATE.get().unwrap().lock();
             s.remove_pin_char();
-            // log_info!("PIN code updated: {}", s.get_pin());
         });
 
         // Mark password is entered
         ui.global::<EnterPinState>().on_passcode_entered(move || {
+            log_info!("Passcode entered");
             let s = STATE.get().unwrap().lock();
             s.mark_unlock_requested();
-            // log_info!("Passcode entered, requesting unlock");
         });
     }
     fn handle_loop_events(&self, ui: &MainWindow, firmware: &mut HitoFirmware) {
@@ -106,14 +100,12 @@ impl CallbackController for EnterPinCallbackController {
         // When the UI marks unlock requested:
         if s.is_unlock_in_progress() {
             // Start job once
-            // log_info!("Starting unlock job");
             if firmware.vault.unlock_job_is_none() {
-                // log_info!("Job is none, starting unlock");
                 let password = s.get_pin();
                 if let Err(e) = firmware.vault.start_unlock(password.as_bytes()) {
-                    // log_info!("Failed to start unlock: {:?}", e);
+                    log_info!("Failed to start unlock: {:?}", e);
                     pin_controller.set_wrong_passcode(true);
-                    pin_controller.invoke_set_progress(-1);
+                    // pin_controller.invoke_set_progress(-1);
                     s.unlock_finished();
                 } else {
                     pin_controller.invoke_set_progress(0);
@@ -124,29 +116,43 @@ impl CallbackController for EnterPinCallbackController {
                 Ok(Some(p)) => {
                     // You can update Slint progress here too, or rely on vault.set_progress callback
                     ui.global::<EnterPinState>().invoke_set_progress(p as i32);
+
+                    if p >= 100 {
+                        // finished successfully
+                        if firmware.vault.is_unlocked() {
+                            log_info!("Unlock finished successfully");
+                            pin_controller.set_wrong_passcode(false);
+                            pin_controller.invoke_set_progress(-1);
+                            s.mark_device_info_requested();
+                            s.unlock_finished();
+                            ui.global::<EnterPinState>().invoke_unlock(true);
+                            firmware.vault.reset_unlock_job();
+                        }
+                    }
                 }
                 Ok(None) => {
+                    log_info!("No progress change this tick");
                     // nothing changed this tick
                 }
                 Err(e) => {
-                    // log_info!("Vault unlock failed: {:?}", e);
+                    log_info!("Vault unlock failed: {:?}", e);
                     // Mark wrong passcode in UI
-                    pin_controller.set_wrong_passcode(true);
-                    pin_controller.invoke_set_progress(0);
-                    s.unlock_finished();
+                    // pin_controller.set_wrong_passcode(true);
+                    // pin_controller.invoke_set_progress(0);
                     ui.global::<EnterPinState>().invoke_unlock(false);
+                    s.unlock_finished();
                 }
             }
-            // If finished successfully, mark UI
-            if firmware.vault.is_unlocked() {
-                // log_info!("Unlock successful");
-                pin_controller.set_wrong_passcode(false);
-                pin_controller.invoke_set_progress(-1);
-                s.mark_device_info_requested();
-                s.unlock_finished();
-                ui.global::<EnterPinState>().invoke_unlock(true);
-                firmware.vault.reset_unlock_job();
-            }
+            // // If finished successfully, mark UI
+            // if firmware.vault.is_unlocked() {
+            //     log_info!("Unlock successful");
+            //     pin_controller.set_wrong_passcode(false);
+            //     pin_controller.invoke_set_progress(-1);
+            //     s.mark_device_info_requested();
+            //     s.unlock_finished();
+            //     ui.global::<EnterPinState>().invoke_unlock(true);
+            //     firmware.vault.reset_unlock_job();
+            // }
         }
     }
 }
